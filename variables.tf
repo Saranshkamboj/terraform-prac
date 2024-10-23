071d4f2a8741a727647a558d343ee3fc2bd3b6bf
Saranshkamboj <90630979+Saranshkamboj@users.noreply.github.com>
2024-10-23 13:19:14 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,6 @@ variable "resource_group_location" {
 
 variable "resource_group_name" {
   type        = string
-  default     = "terraform"
+  default     = "terra"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
